0a21b3bbe271b4880f0a4e9150f9c7bfc952e122
内田麻衣 <maikurodei.biseibutu@gmail.com>
2020-08-14 15:52:01 +0900

    クッキー の円を大きくするコードの追加

---
+++ break_cookies/Assets/Scripts/cookie.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cookie : MonoBehaviour
+{
+    //　円が大きくなるスピード
+    int speed=1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.activeSelf){
+
+            ControllCircle();
+        }
+        
+    }
+
+    void ControllCircle(){
+
+        GameObject circle = gameObject.transform.GetChild(0).gameObject;
+        circle.transform.localScale += new Vector3 (speed,speed,1); 
+    }
+}
+
